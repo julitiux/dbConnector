52a04c180f0b1ac62c089e705fc4488bb6be0405
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-06-27 18:45:03 -0600

    start new proyect dbConnector

---
+++ src/main/java/com/dbConnector/dbConnector/DbConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.dbConnector.dbConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbConnector/dbConnector/DbConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbConnector.dbConnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

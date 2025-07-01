bd482d7b57305df18d8fc9a3da9a96306e0e7e53
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-06-30 21:54:54 -0600

    add modifications to PreguntasEnrollId

--- src/main/java/com/dbConnector/dbConnector/domain/PreguntasEnrollId.java
+++ src/main/java/com/dbConnector/dbConnector/domain/PreguntasEnrollId.java
@@ -1,15 +1,16 @@
 package com.dbConnector.dbConnector.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
-import java.io.Serializable;
-
 @Data
 @Embeddable
-public class PreguntasEnrollId implements Serializable {
+public class PreguntasEnrollId {
 
+  @Column(name = "expediente", length = 20)
   private String expediente;
+  @Column(name = "no_pregunta")
   private Integer noPregunta;
 
 }

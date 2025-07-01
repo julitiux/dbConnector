0d4fc0bd60b1132cb660a7a4743ca4b17596ce9b
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-06-30 21:55:13 -0600

    modify the Id by the entity

--- src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
+++ src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PreguntasEnrollRepository extends JpaRepository<PreguntasEnrollId, PreguntasEnroll> {
+public interface PreguntasEnrollRepository extends JpaRepository<PreguntasEnroll, PreguntasEnrollId> {
 }

5a817f0568b655b9d9d95dc74f7a5b0232fef681
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-06-30 23:33:36 -0600

    add findByIdExpediente

--- src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
+++ src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
@@ -5,6 +5,11 @@ import com.dbConnector.dbConnector.domain.PreguntasEnrollId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PreguntasEnrollRepository extends JpaRepository<PreguntasEnroll, PreguntasEnrollId> {
+
+  List<PreguntasEnroll> findByIdExpediente(String expediente);
+  
 }

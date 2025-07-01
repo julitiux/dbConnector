d20d3124067c11fb93d5430c1a5580f9783ce481
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-06-30 21:55:32 -0600

    add table preguntas_enroll

--- src/main/java/com/dbConnector/dbConnector/repository/CatalogoPreguntaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dbConnector.dbConnector.repository;
-
-import com.dbConnector.dbConnector.domain.CatalogoPregunta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CatalogoPreguntaRepository extends JpaRepository<Integer, CatalogoPregunta> {
-}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -9,3 +9,13 @@ CREATE TABLE people
     last_updated VARCHAR        NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+CREATE TABLE preguntas_enroll (
+    expediente         VARCHAR(20)     NOT NULL,
+    no_pregunta        INTEGER         NOT NULL,
+    respuesta_pregunta VARCHAR(20),
+    estatus            VARCHAR(20),
+    fecha_hora         VARCHAR(20),
+    CONSTRAINT pk_preguntas_enroll PRIMARY KEY (expediente, no_pregunta)
+);

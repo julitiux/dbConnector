4f1b5f8bc71cdee2497146b4306caf289cd92a74
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-01 19:10:55 -0600

    add method to find CatalogoPreguntas by estatusPregunta

--- src/main/java/com/dbConnector/dbConnector/repository/CatalogoPreguntasRepository.java
+++ src/main/java/com/dbConnector/dbConnector/repository/CatalogoPreguntasRepository.java
@@ -4,6 +4,11 @@ import com.dbConnector.dbConnector.domain.CatalogoPreguntas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CatalogoPreguntasRepository extends JpaRepository<CatalogoPreguntas, Integer> {
+
+  List<CatalogoPreguntas> findByEstatusPregunta(String estatusPregunta);
+
 }

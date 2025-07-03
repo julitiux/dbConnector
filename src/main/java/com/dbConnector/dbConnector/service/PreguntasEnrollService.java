8e80a04eb41d58988ea67d50369669374ddaca00
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-02 23:27:04 -0600

    add method to retrieve and shuffle preguntas enroll IDs by expediente

--- src/main/java/com/dbConnector/dbConnector/service/PreguntasEnrollService.java
+++ src/main/java/com/dbConnector/dbConnector/service/PreguntasEnrollService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -41,5 +42,15 @@ public class PreguntasEnrollService {
       pregunta.setFechaHora(LocalDateTime.now().format(formatter));
     });
     return preguntasEnrollRepository.saveAll(preguntasEnrolls);
+  }
+
+  public List<Integer> getPreguntasEnrollByExpediente(String expediente) {
+
+    List<PreguntasEnroll> preguntasEnrolls = preguntasEnrollRepository.findByIdExpediente(expediente);
+    List<Integer> preguntasIds = new java.util.ArrayList<>(preguntasEnrolls.stream()
+      .map(pregunta -> pregunta.getId().getNoPregunta())
+      .toList());
+    Collections.shuffle(preguntasIds);
+    return preguntasIds.stream().limit(3).toList();
   }
 }

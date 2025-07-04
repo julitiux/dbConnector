477df763c2b1900ea81609e2a39035595084573b
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-04 13:52:53 -0600

    remove people table definition from initialization script

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,14 +1,3 @@
-CREATE TABLE people
-(
-    id           UUID           NOT NULL,
-    username     VARCHAR UNIQUE NOT NULL,
-    name         VARCHAR        NOT NULL,
-    lastname     VARCHAR        NOT NULL,
-    password     VARCHAR        NOT NULL,
-    date_created VARCHAR        NOT NULL,
-    last_updated VARCHAR        NOT NULL,
-    PRIMARY KEY (id)
-);
 
 CREATE TABLE sd_catalogo_preguntas (
     no_pregunta         INTEGER PRIMARY KEY,

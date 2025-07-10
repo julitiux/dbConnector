d584653f3ea529713e608af12916fc213ea12387
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-10 14:31:50 -0600

    complement to method mapToPreguntasEnrollToSave. Implement method getFechaHora and delete some properties to method mapToPreguntasEnrollToCompare

--- src/main/java/com/dbConnector/dbConnector/mapper/IWrapperPostEnrollmentQuestionnaireRequest.java
+++ src/main/java/com/dbConnector/dbConnector/mapper/IWrapperPostEnrollmentQuestionnaireRequest.java
@@ -5,7 +5,10 @@ import com.dbConnector.dbConnector.model.AnswersDetailsQuestion;
 import com.dbConnector.dbConnector.model.WrapperPostEnrollmentQuestionnaireRequest;
 import org.mapstruct.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 @Mapper(componentModel = "spring",
   nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
@@ -14,13 +17,31 @@ import java.util.List;
 public interface IWrapperPostEnrollmentQuestionnaireRequest {
 
   @Mapping(target = "id.noPregunta", source = "question.questionId")
+  @Mapping(target = "respuestaPregunta", source = "question.answer")
+  PreguntasEnroll mapToPreguntasEnrollToCompare(AnswersDetailsQuestion question);
+
+  @Mapping(target = "id.noPregunta", source = "question.questionId")
   @Mapping(target = "id.expediente", source = "expediente")
   @Mapping(target = "respuestaPregunta", source = "question.answer")
-  PreguntasEnroll mapToPreguntasEnroll(AnswersDetailsQuestion question, String expediente);
+  @Mapping(target = "estatus",  constant = "A")
+  @Mapping(target = "fechaHora", expression = "java(this.getFechaHora())")
+  PreguntasEnroll mapToPreguntasEnrollToSave(AnswersDetailsQuestion question, String expediente);
 
-  default List<PreguntasEnroll> mapToPreguntasEnrollList(WrapperPostEnrollmentQuestionnaireRequest request, String expediente) {
+  default List<PreguntasEnroll> mapToPreguntasEnrollListToCompare(WrapperPostEnrollmentQuestionnaireRequest request) {
     return request.getQuestions().stream().map(question -> {
-      return mapToPreguntasEnroll(question.getQuestion(), expediente);
+      return mapToPreguntasEnrollToCompare(question.getQuestion()) ;
     }).toList();
+  }
+
+  default List<PreguntasEnroll> mapToPreguntasEnrollListToSave(WrapperPostEnrollmentQuestionnaireRequest request, String expediente) {
+    return request.getQuestions().stream().map(question -> {
+      return mapToPreguntasEnrollToSave(question.getQuestion(), expediente) ;
+    }).toList();
+  }
+
+  @Named("getFechaHora")
+  default String getFechaHora(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy:HHmmss", Locale.ENGLISH);
+    return LocalDateTime.now().format(formatter);
   }
 }

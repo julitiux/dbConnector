fd0bfdb7a457d056196a50b95b84b0c7e4c046a2
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-10 17:50:38 -0600

    modify the implementation of method createPreguntasEnroll, before return a list of PreguntasEnroll, now return a Boolean

--- src/main/java/com/dbConnector/dbConnector/service/PreguntasEnrollService.java
+++ src/main/java/com/dbConnector/dbConnector/service/PreguntasEnrollService.java
@@ -1,48 +1,40 @@
 package com.dbConnector.dbConnector.service;
 
 import com.dbConnector.dbConnector.domain.PreguntasEnroll;
-import com.dbConnector.dbConnector.mapper.IWrapperPostEnrollmentQuestionnaireRequest;
+import com.dbConnector.dbConnector.mapper.IWrapperPostEnrollmentQuestionnaireMapper;
 import com.dbConnector.dbConnector.model.WrapperPostEnrollmentQuestionnaireRequest;
 import com.dbConnector.dbConnector.repository.PreguntasEnrollRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class PreguntasEnrollService {
 
   private final PreguntasEnrollRepository preguntasEnrollRepository;
-  private final IWrapperPostEnrollmentQuestionnaireRequest answerDetailsQuestion;
-
-  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy:HHmmss", Locale.ENGLISH);
-
-  public PreguntasEnrollService(PreguntasEnrollRepository preguntasEnrollRepository, IWrapperPostEnrollmentQuestionnaireRequest answerDetailsQuestion) {
-    this.preguntasEnrollRepository = preguntasEnrollRepository;
-    this.answerDetailsQuestion = answerDetailsQuestion;
-  }
+  private final IWrapperPostEnrollmentQuestionnaireMapper iWrapperPostEnrollmentQuestionnaireMapper;
 
   public Boolean validatePreguntasEnroll(WrapperPostEnrollmentQuestionnaireRequest request, String expediente) {
 
-    List<PreguntasEnroll> preguntasEnrolls = answerDetailsQuestion.mapToPreguntasEnrollListToCompare(request);
+    List<PreguntasEnroll> preguntasEnrolls = iWrapperPostEnrollmentQuestionnaireMapper.mapToPreguntasEnrollListToCompare(request);
     List<PreguntasEnroll> preguntasEnrollsSaved = preguntasEnrollRepository.findByIdExpediente(expediente);
-
     return preguntasEnrollsSaved.equals(preguntasEnrolls);
   }
 
-  public List<PreguntasEnroll> createPreguntasEnroll(List<PreguntasEnroll> preguntasEnrolls, String expediente) {
+  public Boolean createPreguntasEnroll(WrapperPostEnrollmentQuestionnaireRequest request, String expediente) {
 
-    preguntasEnrolls.forEach(pregunta -> {
-      pregunta.getId().setExpediente(expediente);
-      pregunta.setEstatus("A");
-      pregunta.setFechaHora(LocalDateTime.now().format(formatter));
-    });
-    return preguntasEnrollRepository.saveAll(preguntasEnrolls);
+    List<PreguntasEnroll> preguntasEnrolls = iWrapperPostEnrollmentQuestionnaireMapper.mapToPreguntasEnrollListToSave(request, expediente);
+    List<PreguntasEnroll> preguntasEnrollsCreated = preguntasEnrollRepository.saveAll(preguntasEnrolls);
+    if (preguntasEnrollsCreated != null && !preguntasEnrollsCreated.isEmpty()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public Boolean deletePreguntasEnrollByExpediente(String expediente) {

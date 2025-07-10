f260d1c2b0e8c0b762ef76aaa46c77e041c4653b
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-09 18:03:55 -0600

    add EqualsAndHashCode annotation to PreguntasEnrollId for improved object comparison

--- src/main/java/com/dbConnector/dbConnector/domain/PreguntasEnrollId.java
+++ src/main/java/com/dbConnector/dbConnector/domain/PreguntasEnrollId.java
@@ -3,14 +3,16 @@ package com.dbConnector.dbConnector.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Embeddable
+@EqualsAndHashCode(of = "noPregunta")
 public class PreguntasEnrollId {
 
   @Column(name = "expediente", length = 20)
   private String expediente;
+
   @Column(name = "no_pregunta")
   private Integer noPregunta;
-
 }

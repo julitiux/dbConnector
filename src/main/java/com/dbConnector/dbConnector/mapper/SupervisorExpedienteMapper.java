ddb507327cb656d90c07b8ee1d5c1ed9c364f1e0
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-11 15:20:23 -0600

    implement method default mapToWrapperRetrieveSubordinateResponse

--- src/main/java/com/dbConnector/dbConnector/mapper/SupervisorExpedienteMapper.java
+++ src/main/java/com/dbConnector/dbConnector/mapper/SupervisorExpedienteMapper.java
@@ -1,11 +1,26 @@
 package com.dbConnector.dbConnector.mapper;
 
+import com.dbConnector.dbConnector.domain.SupervisorExpediente;
+import com.dbConnector.dbConnector.model.response.ListSubordinate;
+import com.dbConnector.dbConnector.model.response.ListSubordinatesEmployee;
+import com.dbConnector.dbConnector.model.response.WrapperRetrieveSubordinateResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueMappingStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring",
   nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
   nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT)
 public interface SupervisorExpedienteMapper {
+
+  default WrapperRetrieveSubordinateResponse mapToWrapperRetrieveSubordinateResponse(List<SupervisorExpediente> supervisorExpedientes) {
+
+    List<ListSubordinate> subordinates = supervisorExpedientes.stream()
+      .map(supervisorExpediente -> {
+        return new ListSubordinate(new ListSubordinatesEmployee(supervisorExpediente.getExpAgente()));
+      }).toList();
+    return new WrapperRetrieveSubordinateResponse(subordinates);
+  }
 }

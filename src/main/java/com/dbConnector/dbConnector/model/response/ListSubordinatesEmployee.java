3d58cc3f8cd9e054cdbd3ad335c382124409535c
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-11 15:21:01 -0600

    add constructor on class ListSubordinate, ListSubordinatesEmployee and WrapperRetrieveSubordinateResponse

--- src/main/java/com/dbConnector/dbConnector/model/response/ListSubordinatesEmployee.java
+++ src/main/java/com/dbConnector/dbConnector/model/response/ListSubordinatesEmployee.java
@@ -1,8 +1,10 @@
 package com.dbConnector.dbConnector.model.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ListSubordinatesEmployee {
 
   private String number;
--- src/main/java/com/dbConnector/dbConnector/model/response/WrapperRetrieveSubordinateResponse.java
+++ src/main/java/com/dbConnector/dbConnector/model/response/WrapperRetrieveSubordinateResponse.java
@@ -1,9 +1,13 @@
 package com.dbConnector.dbConnector.model.response;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
+@AllArgsConstructor
 public class WrapperRetrieveSubordinateResponse {
 
   private List<ListSubordinate> subordinates;

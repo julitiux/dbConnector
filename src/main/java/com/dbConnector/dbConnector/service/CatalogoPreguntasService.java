66e395ba0a385d525e086852f6934dba4731e121
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-10 19:37:37 -0600

    add tag AllArgsConstructor to eliminate the explicit contructor

--- src/main/java/com/dbConnector/dbConnector/service/CatalogoPreguntasService.java
+++ src/main/java/com/dbConnector/dbConnector/service/CatalogoPreguntasService.java
@@ -2,19 +2,17 @@ package com.dbConnector.dbConnector.service;
 
 import com.dbConnector.dbConnector.domain.CatalogoPreguntas;
 import com.dbConnector.dbConnector.repository.CatalogoPreguntasRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class CatalogoPreguntasService {
 
   private final CatalogoPreguntasRepository catalogoPreguntasRepository;
-
-  public CatalogoPreguntasService(CatalogoPreguntasRepository catalogoPreguntasRepository) {
-    this.catalogoPreguntasRepository = catalogoPreguntasRepository;
-  }
 
   public List<CatalogoPreguntas> getPreguntasByEstatus(String estatusPregunta) {
     List<CatalogoPreguntas> catalogoPreguntas = catalogoPreguntasRepository.findByEstatusPregunta(estatusPregunta);

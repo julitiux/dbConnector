a401f2bec89cd71d906cec635cc1a35f4c5500b8
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-11 16:34:28 -0600

    add new bean ISupervisorExpedienteMapper and delete the contructor with tag AllArgsConstructor

--- src/main/java/com/dbConnector/dbConnector/service/SupervisorExpedienteService.java
+++ src/main/java/com/dbConnector/dbConnector/service/SupervisorExpedienteService.java
@@ -1,25 +1,24 @@
 package com.dbConnector.dbConnector.service;
 
 import com.dbConnector.dbConnector.domain.SupervisorExpediente;
+import com.dbConnector.dbConnector.mapper.ISupervisorExpedienteMapper;
+import com.dbConnector.dbConnector.model.response.WrapperRetrieveSubordinateResponse;
 import com.dbConnector.dbConnector.repository.SupervisorExpedienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class SupervisorExpedienteService {
 
   final private SupervisorExpedienteRepository supervisorExpedienteRepository;
+  final private ISupervisorExpedienteMapper iSupervisorExpedienteMapper;
 
-  public SupervisorExpedienteService(SupervisorExpedienteRepository supervisorExpedienteRepository) {
-    this.supervisorExpedienteRepository = supervisorExpedienteRepository;
-  }
-
-  public List<String> getSubordinariosId(String expediente) {
+  public WrapperRetrieveSubordinateResponse getSubordinariosId(String expediente) {
 
     List<SupervisorExpediente> supervisorExpedientes = supervisorExpedienteRepository.findByExpediente(expediente);
-
-    return supervisorExpedientes.stream().map(SupervisorExpediente::getExpAgente).toList();
+    return iSupervisorExpedienteMapper.mapToWrapperRetrieveSubordinateResponse(supervisorExpedientes);
   }
-
 }

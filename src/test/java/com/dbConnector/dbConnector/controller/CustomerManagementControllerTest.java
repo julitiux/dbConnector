3af22e715f3915c405efab6f6a1f74b4a5d94bbd
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-17 13:00:11 -0600

    add test testProcessValidatePreguntasEnrollWhenReturnOk

--- src/test/java/com/dbConnector/dbConnector/controller/CustomerManagementControllerTest.java
+++ src/test/java/com/dbConnector/dbConnector/controller/CustomerManagementControllerTest.java
@@ -1,13 +1,43 @@
 package com.dbConnector.dbConnector.controller;
 
+import com.dbConnector.dbConnector.model.request.WrapperPostEnrollmentQuestionnaireRequest;
+import com.dbConnector.dbConnector.service.CatalogoPreguntasService;
+import com.dbConnector.dbConnector.service.PreguntasEnrollService;
+import com.dbConnector.dbConnector.service.SupervisorExpedienteService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerManagementControllerTest {
 
+  @Mock
+  private PreguntasEnrollService preguntasEnrollService;
+  @Mock
+  private CatalogoPreguntasService catalogoPreguntasService;
+  @Mock
+  private SupervisorExpedienteService supervisorExpedienteService;
+  @InjectMocks
+  CustomerManagementController customerManagementController;
+
   @Test
-  void processValidatePreguntasEnroll() {
+  void testProcessValidatePreguntasEnrollWhenReturnOk() {
+
+    final String employee_id = "";
+    final WrapperPostEnrollmentQuestionnaireRequest request = new WrapperPostEnrollmentQuestionnaireRequest();
+    when(preguntasEnrollService.validatePreguntasEnroll(request, employee_id)).thenReturn(Boolean.TRUE);
+
+    ResponseEntity<Void> response = customerManagementController.processValidatePreguntasEnroll(employee_id, request);
+
+    assertEquals(200, response.getStatusCode().value(), "should be return HTTP 200");
+    assertFalse(response.hasBody(), "dont has body");
   }
 
   @Test

bfb5377fe05f9f913e793466cbc14967d4f69798
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-16 18:22:51 -0600

    add test CustomerManagementControllerTest

---
+++ src/test/java/com/dbConnector/dbConnector/controller/CustomerManagementControllerTest.java
@@ -0,0 +1,32 @@
+package com.dbConnector.dbConnector.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerManagementControllerTest {
+
+  @Test
+  void processValidatePreguntasEnroll() {
+  }
+
+  @Test
+  void createPreguntasEnroll() {
+  }
+
+  @Test
+  void getCatalogoPreguntasByEstatus() {
+  }
+
+  @Test
+  void getSubordinadosByExpediente() {
+  }
+
+  @Test
+  void getPreguntasEnrollByExpediente() {
+  }
+
+  @Test
+  void deletePreguntasEnrollByExpediente() {
+  }
+}

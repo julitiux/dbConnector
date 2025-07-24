980fdb76ed7b122fe3989c61acad5f163311d218
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-07-23 22:01:01 -0600

    add @Query to get noPregunta and descPregunta

--- src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
+++ src/main/java/com/dbConnector/dbConnector/repository/PreguntasEnrollRepository.java
@@ -2,7 +2,10 @@ package com.dbConnector.dbConnector.repository;
 
 import com.dbConnector.dbConnector.domain.PreguntasEnroll;
 import com.dbConnector.dbConnector.domain.PreguntasEnrollId;
+import com.dbConnector.dbConnector.model.domain.PreguntaDescripcionDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +14,9 @@ import java.util.List;
 public interface PreguntasEnrollRepository extends JpaRepository<PreguntasEnroll, PreguntasEnrollId> {
 
   List<PreguntasEnroll> findByIdExpediente(String expediente);
-  
+
+  @Query("SELECT new com.dbConnector.dbConnector.model.domain.PreguntaDescripcionDTO(p.id.noPregunta, p.catalogoPreguntas.descPregunta) " +
+    "FROM PreguntasEnroll p " +
+    "WHERE p.id.expediente = :expediente")
+  List<PreguntaDescripcionDTO> findPreguntasYDescripcionByExpediente(@Param("expediente") String expediente);
 }
